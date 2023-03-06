e23e443ff977e5c0d8db3b37617e137bb11c5121
Denys Nahernyak <denchik16062001@gmail.com>
2023-03-06 23:13:12 +0200

    [Jobs] Add capacitor, that "converts" Angular app to Native IOS and Android apps

---
+++ jobs/android/app/src/main/java/dennissimmo/jobs/MainActivity.java
@@ -0,0 +1,6 @@
+package dennissimmo.jobs;
+
+import com.getcapacitor.BridgeActivity;
+// denchik16062001@gmail.com
+// Test16062001
+public class MainActivity extends BridgeActivity {}
---
+++ jobs/capacitor.config.ts
@@ -0,0 +1,11 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dennissimmo.jobs',
+  appName: 'jobs',
+  webDir: 'dist/jobs',
+
+  bundledWebRuntime: false
+};
+
+export default config;
